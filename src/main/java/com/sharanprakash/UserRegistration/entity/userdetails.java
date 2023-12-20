d5c02f93dbff7d3bfb77649659d83fb8b40905c0
unknown <devCynosure>
2023-12-20 23:34:28 +0530

    Added bootstrap registration Form

--- src/main/java/com/sharanprakash/UserRegistration/entity/userdetails.java
+++ src/main/java/com/sharanprakash/UserRegistration/entity/userdetails.java
@@ -1,2 +1,66 @@
-package com.sharanprakash.UserRegistration.entity;public class userdetails {
+package com.sharanprakash.UserRegistration.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "USER_DTLS")
+public class userdetails {
+
+    private int id;
+    private String fullname;
+    private String email;
+    private String password;
+    private int tos;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getTos() {
+        return tos;
+    }
+
+    public void setTos(int tos) {
+        this.tos = tos;
+    }
+
+    @Override
+    public String toString() {
+        return "userdetails{" +
+                "id=" + id +
+                ", fullname='" + fullname + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", tos=" + tos +
+                '}';
+    }
 }
